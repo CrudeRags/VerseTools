7f799aed84719add5d948ab4babbf5362cb7fb0f
Crude Rags <crude.rags@gmail.com>
2020-04-12 19:49:22 +0530

    Added file_tools
    Now verse references and EGW references can be extracted from files. You can get the extracted verses in different languages

--- verse_tools/config.py
+++ verse_tools/config.py
@@ -1,27 +1,29 @@
 import requests
 import json
 import os
-import shutil
 
-def save_config(pref_path,pref):
+
+def save_config(pref_path, pref):
     with open(pref_path, 'w', encoding='utf8') as f:
-        json.dump(pref,f,indent=2,sort_keys=True)
-    
+        json.dump(pref, f, indent=2, sort_keys=True)
+
     return
+
 
 def default_config(pref_path):
-    config_string = """{
-  "default_language": "English",
-  "git_languages": []
-}"""
+    config_string = """{"default_language": "English",
+  "git_languages": []}"""
     config = json.loads(config_string)
 
-    save_config(pref_path,config)
+    save_config(pref_path, config)
     return
 
-def refresh(pref_path,pref):
-    git = requests.get('https://api.github.com/repos/CrudeRags/Bible-database/commits')
-    trl = git.json()[0]['commit']['tree']['url']  # url of tree in the latest commit
+
+def refresh(pref_path, pref):
+    git = requests.get(
+        'https://api.github.com/repos/CrudeRags/Bible-database/commits')
+    # url of tree in the latest commit
+    trl = git.json()[0]['commit']['tree']['url']
     trl_get = requests.get(trl)
 
     my_languages = []
@@ -31,38 +33,26 @@ def refresh(pref_path,pref):
             my_languages.append(x['path'].split('Bible')[0])
 
     pref['git_languages'] = my_languages
-    save_config(pref_path,pref)
+    save_config(pref_path, pref)
     return
 
-def set_resource_path(pref_path,pref):
+
+def set_resource_path(pref_path, pref):
     print("\nResource path should not contain spaces. Use _ instead\
             \ncurrent resources will be moved to the new directory")
     print("If you uninstall this package, resource directory has\
             \nto be manually removed")
     new_resource_path = input("\nEnter resource path: ")
-    
+
     if not os.path.exists(new_resource_path):
         os.makedirs(new_resource_path)
-    
+
     pref['resource_path'] = new_resource_path
 
-    save_config(pref_path,pref)
-    
-    return 
+    save_config(pref_path, pref)
 
-#def move_resources(pref_path,pref):
-    
- #   old_path = pref['resource_path'] 
+    return
 
-  #  set_resource_path(pref_path,pref)
-
-   # new_path = pref['resource_path']
-
-    #print("moving resources to the set directory")
-    
-    #_ = shutil.move(old_path,new_path)
-
-   # return
 
 def main():
     file_path = os.path.abspath(os.path.dirname(__file__))
@@ -71,11 +61,12 @@ def main():
     if not os.path.isfile(_pref_path_):
         default_config(_pref_path_)
 
-    with open(_pref_path_,'r') as p:
+    with open(_pref_path_, 'r') as p:
         pref = json.load(p)
 
-    refresh(_pref_path_,pref)
-    set_resource_path(_pref_path_,pref)
+    refresh(_pref_path_, pref)
+    set_resource_path(_pref_path_, pref)
+
 
 if __name__ == "__main__":
     main()
--- verse_tools/file_tools.py
+++ verse_tools/file_tools.py
@@ -3,9 +3,9 @@ from verse_tools import GetVerses as gv
 
 
 def extract(file_string):
-    refPattern = re.compile(r'(\d* \w+ \d+:\s*\d+[\d\s,;]*)')
+    refPattern = re.compile(r'(\d* *\w+\s+\d+:\s*\d+[-\d\s:,;]*|\d+:\s*\d+[-\d\s,:;]*)')
     my_refs = refPattern.findall(file_string)
-    return [x for x in map(str.strip, my_refs)]
+    return [x.strip(";,") for x in map(str.strip, my_refs)]
 
 
 def extract_paragraph_refs(file_string):
@@ -14,6 +14,33 @@ def extract_paragraph_refs(file_string):
     return [x for x in map(str.strip, my_refs)]
 
 
-def getExtractedVerses(refs, lang="English"):
-    g = gv.GetVerses(lang=lang)
-    return g.get
+def __getVerses__(refs, lang):
+    g = gv.GetVerses(lang=lang)   
+    return g.get(refs)
+
+def get(refs, lang=None, output=None):
+    out_list = []
+
+    if lang is not None:
+        langs = lang.split(',')
+        for l in langs:
+            out_list.append(__getVerses__(refs,l.strip()))
+    else:
+        lang = gv.get_pref()['default_language']
+        out_list.append(__getVerses__(refs,lang))
+
+    x = len(out_list)
+    y = len(out_list[0])
+    z = [(b, a) for a in range(x) for b in range(y)]
+    z.sort()
+
+    if output is not None:
+        with open(output, 'w+', encoding='utf8') as out:
+            for b, a in z:
+                out.write(out_list[a][b]['ref'] + " " +
+                          out_list[a][b]['verse'] + '\n')
+
+    else:
+        for b, a in z:
+            print(out_list[a][b]['ref'] + '\n' + out_list[a][b]['verse'])
+            print()
